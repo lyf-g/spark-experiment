906a78fdb5ed9abe3fae995172d4de84a9a34a9d
lyf <1141730046@qq.com>
2024-12-04 22:46:55 +0800

    Modifying the entry file

--- scripts/process.py
+++ scripts/process.py
@@ -1,6 +1,13 @@
 from pyspark import SparkFiles
 from pyspark.sql import SparkSession
 import os
+from custom_modules import log_process
+
+server_host = "hdfs://hadoop01:9000"
+log_directory = f"{server_host}/data/perfdata/"
+log_output_path = f"{server_host}/data/csvs/mysqld_micro.csv"
+sysbench_directory = f"{server_host}/data/script_output/"
+
 
 def list_hdfs_files(spark, directory):
     fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration())
@@ -10,26 +17,20 @@ def list_hdfs_files(spark, directory):
         print(f"路径 {directory} 不存在")
         return []
     
-    # 获取目录下的所有文件
     files = fs.listStatus(path)
     file_list = [file.getPath().toString() for file in files if fs.isFile(file.getPath())]
     return file_list
+
+def solve_log_files(spark, directory, output_path):
+    file_list = list_hdfs_files(spark, directory)
+    df = log_process.process(spark, file_list,output_path)
+
 
 if __name__ == "__main__":
     spark = SparkSession.builder \
         .appName("ListHDFSFiles") \
         .getOrCreate()
 
-    hdfs_directory = "hdfs://10.24.7.59:9000/data/perfdata"  # 修改为目标路径
-    print(f"正在列出目录 {hdfs_directory} 下的文件...")
-    # print(hdfs_directory)
-    files = list_hdfs_files(spark, hdfs_directory)
+    solve_log_files(spark, log_directory, log_output_path)
 
-    if files:
-        print("文件列表：")
-        for file in files:
-            print(file)
-    else:
-        print("未找到文件或目录不存在。")
-    
     spark.stop()
